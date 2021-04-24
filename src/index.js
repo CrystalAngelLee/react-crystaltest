b99c31c044cb2f176ecb5fac7cf25f60e685e68e
CrystalAngelLee <crystaljiaxuan@163.com>
2021-04-24 22:48:02 +0800

    commit react16.13

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log(123);
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"), function () {
+  console.log("callback")
+})
